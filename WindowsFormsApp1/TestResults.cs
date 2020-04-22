47f64d56a286797590ff09dd22434fa61076b5ff
AJ <ajisaacs27@gmail.com>
2020-04-22 17:47:58 -0400

    Added test results class.

---
+++ WindowsFormsApp1/TestResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    public class TestResults
+    {
+        public long ReactionTimeMilliseconds { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string Message { get; set; }
+    }
+}
